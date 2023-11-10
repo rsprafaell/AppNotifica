1bac5c378524da3b6dfcc5044b905d125faae0b6
rsprafaell <rsprafaell@hotmail.com>
2023-11-10 11:06:06 -0300

    Initial Commit

---
+++ AppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica
+//
+//  Created by IFB-BIOTIC-23 on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
